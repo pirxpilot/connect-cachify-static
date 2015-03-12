3a8b7c4b34240378dcbf6341f37e55ec0fccf773
Damian Krzeminski <pirxpilot@code42day.com>
2015-03-12 10:25:22 -0400

    fix: works correctly when no options are passed

--- test/cachify-static.js
+++ test/cachify-static.js
@@ -5,41 +5,46 @@ var serveStatic = require('serve-static');
 var fixtures = __dirname + '/fixtures';
 var request = require('./support/http');
 
-var app = connect();
 
-/* global describe, it */
+/* global describe, it, before */
 
 
-app.use(cachifyStatic(fixtures, {
-  match: /\.css$|\.txt$/,
-  control_headers: true
-}));
-app.use(serveStatic(fixtures));
+describe('cachifyStatic custom config', function(){
+  before(function() {
+    var app = connect();
 
-app.use(function(req, res){
-  res.statusCode = 404;
-  res.end('sorry!');
-});
+    app.use(cachifyStatic(fixtures, {
+      match: /\.css$|\.txt$/,
+      control_headers: true
+    }));
+    app.use(serveStatic(fixtures));
 
-describe('cachifyStatic', function(){
+    app.use(function(req, res){
+      res.statusCode = 404;
+      res.end('sorry!');
+    });
+
+    this.app = app;
+  });
+
   it('should serve static files', function(done){
     var url = cachifyStatic.cachify('/a.css');
 
     url.should.be.eql('/9a6f75849b/a.css');
 
-    request(app)
+    request(this.app)
     .get(url)
     .expect('1', done);
   });
 
   it('should set cache headers', function(done){
-    request(app)
+    request(this.app)
     .get(cachifyStatic.cachify('/a.css'))
     .expect('Cache-Control', 'public, max-age=31536000', done);
   });
 
   it('should strip other headers', function(done){
-    request(app)
+    request(this.app)
     .get(cachifyStatic.cachify('/a.css'))
     .end(function(res) {
       res.headers.should.not.have.property('etag');
@@ -48,15 +53,57 @@ describe('cachifyStatic', function(){
   });
 
   it('should not mess not cachified files', function(done){
-    request(app)
+    request(this.app)
     .get('/texts/b.txt')
     .expect('2', done);
   });
 
   it('should ignore wrong hashes', function(done){
-    request(app)
+    request(this.app)
     .set('Accept-Encoding', 'gzip')
     .post('/0123456789/a.css')
     .expect(404, done);
   });
+});
+
+describe('cachifyStatic default config', function(){
+  before(function() {
+    var app = connect();
+
+    app.use(cachifyStatic(fixtures));
+    app.use(serveStatic(fixtures));
+
+    app.use(function(req, res){
+      res.statusCode = 404;
+      res.end('sorry!');
+    });
+
+    this.app = app;
+  });
+
+  it('should serve static files', function(done){
+    var url = cachifyStatic.cachify('/a.css');
+
+    url.should.be.eql('/9a6f75849b/a.css');
+
+    request(this.app)
+    .get(url)
+    .expect('1', done);
+  });
+
+  it('should set cache headers', function(done){
+    request(this.app)
+    .get(cachifyStatic.cachify('/a.css'))
+    .expect('Cache-Control', 'public, max-age=31536000', done);
+  });
+
+  it('should not strip other headers', function(done){
+    request(this.app)
+    .get(cachifyStatic.cachify('/a.css'))
+    .end(function(res) {
+      res.headers.should.have.property('etag');
+      done();
+    });
+  });
+
 });
